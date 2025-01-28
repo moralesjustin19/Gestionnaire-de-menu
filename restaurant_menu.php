1ce30c384559156cb10254c2ebbdd5351c70a149
HEUREUX Axel <axel.heureux@laplateforme.io>
2025-01-28 15:38:17 +0100

    Correction d'un mauvais commit

--- restaurant_menu.php
+++ restaurant_menu.php
@@ -1,17 +1,4 @@
 <?php
-
-$servername = "localhost";
-$username = "root";
-$password ="";
-$dbname ="gestionnaire_menu";
-
-try {
-    $bdd = new PDO("mysql:host=$servername;dbname=gestionnaire_menu", $username, $password);
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    
-    echo "Erreur de connexion : " . $e->getMessage();
-}
 ?>
 
 <!DOCTYPE html>
