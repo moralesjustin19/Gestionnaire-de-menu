9ef9fdd5eef232588084cb4f327084e608bf6c9d
moralesjustin19 <justin.mogrovejo@laplateforme.io>
2025-01-28 15:02:07 +0100

    connection a la base de données sur la page d'affichage

--- restaurant_menu.php
+++ restaurant_menu.php
@@ -1,4 +1,17 @@
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname ="gestionnaire_menu";
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=gestionnaire_menu", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    
+    echo "Erreur de connexion : " . $e->getMessage();
+}
 ?>
 
 <!DOCTYPE html>
