8d648ba60597deec5d6a72ea619d8ed74c3914cc
HEUREUX Axel <axel.heureux@laplateforme.io>
2025-01-30 15:10:29 +0100

    🐛 Fix desync CSS page gerer

--- gerer.php
+++ gerer.php
@@ -32,7 +32,7 @@ $menus = $requeteMenus->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylesheet/gestion.css">
+    <link rel="stylesheet" href="stylesheet/gerer.css">
     <title>Gestion du Menu</title>
 </head>
 <body>
